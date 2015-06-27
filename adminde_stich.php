6751e25b6bbda5cec5bba782b49a445fe635ff60
Xumanta <Xumanta@users.noreply.github.com>
2015-06-27 12:51:20 +0200

    Add Comments

--- adminde_stich.php
+++ adminde_stich.php
@@ -1,16 +1,15 @@
 <?php
 set_include_path('inc');
-$mysql_database = "City_Guide";
-include "db_connect.inc.php";
-include "function_save_sql.php";
+$mysql_database = "City_Guide";   // Set used Database for Output
+include "db_connect.inc.php";	 // Universal connect
+include "function_save_sql.php"; // For secure Database Queries
 
-if(isset($_POST['stichwort']))
-{
-	$stich = save_sql($_POST['stichwort']);
-	$sqlEintrag = "INSERT INTO `Stichwort` (`StichwortID`, `Keywords`) VALUES ('NULL', '$stich')";
+if(isset($_POST['stichwort'])) {
+	$stich = save_sql($_POST['stichwort']); // If a Keyword was added
+	$sqlEintrag = "INSERT INTO `Stichwort` (`StichwortID`, `Keywords`) VALUES ('NULL', '$stich')"; // Add Query
 	mysqli_query($db, $sqlEintrag);
 
-	header("Location: adminde_stich.php");
+	header("Location: adminde_stich.php"); // Header on this File for cleaning the Post cache
 }
 ?>
 <!DOCTYPE html>
@@ -32,10 +31,9 @@ if(isset($_POST['stichwort']))
 		<?php
 			$sqlAusgabe = "SELECT * FROM `Stichwort`;";
 			$tabelle = mysqli_query($db, $sqlAusgabe);
-			while ($fetched = mysqli_fetch_array($tabelle))
-			{
+			while ($fetched = mysqli_fetch_array($tabelle)) {
 				print "$fetched['Keywords'] <br/>";
-			}
+			} // Prints all possible Keywords
 		?>
 	</body>
 </html>
