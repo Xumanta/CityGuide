c58b548e7e07399c32c320587f42ac4730c8c2f9
Xumanta <Xumanta@users.noreply.github.com>
2015-06-27 12:43:35 +0200

    Add Comments

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 set_include_path('inc');
-$mysql_database = "City_Guide";
-include "db_connect.inc.php";
-include "function_save_sql.php";
-include "function_editKey.php";
+$mysql_database = "City_Guide";  // Set used Database for Output
+include "db_connect.inc.php";	 // Universal connect
+include "function_save_sql.php"; // For secure Database Queries
+include "function_editKey.php";  // Makes Search better
 
-$gesucht = false;
+$gesucht = false; // For Checking if everything have to be printed
 if (isset($_POST["Suche"])) $gesucht = true;
 
 ?>
@@ -38,8 +38,8 @@ if (isset($_POST["Suche"])) $gesucht = true;
     
     <div class="content">
 		<?php
-        if (!$gesucht) { // Damit nicht immer alles ausgegeben wird
-            $sqlAus = "SELECT * FROM `Guide`"; // SELECT Befehl
+        if (!$gesucht) { // Check for Search
+            $sqlAus = "SELECT * FROM `Guide`"; // SELECT Query for All Entries
 			$teildb = mysqli_query($db, $sqlAus);
 			while ($fetched = mysqli_fetch_array($teildb))
             {
@@ -49,13 +49,13 @@ if (isset($_POST["Suche"])) $gesucht = true;
                         <p><b>Nächste Bushaltestelle:</b><br> ".$fetched['Bus_stop']."</p><br>
                         <p><b>Kurzbeschreibung:</b><br>".$fetched['Abstract']."</p><br>
                         <p><b>Auf der Karte:</b><br> ".$fetched['Google_Maps']."</p>
-                    </div>";
+                    </div>"; // Print Query Entries
             }
         } else {
-            $sucht = editKey(save_sql($_POST["Suche"]));
+            $sucht = editKey(save_sql($_POST["Suche"])); // Edit of Search Word for Secure Query and better Search
             // $sqlSearch = "SELECT * FROM 'Stichwort' WHERE 'Keywords'='$sucht' INNER JOIN 'Suche' USING (StichwortID) INNER JOIN 'Guide' USING (GuideID);";
            // $sqlSearch = "SELECT * FROM 'Stichwort' WHERE 'Keywords'='$sucht' RIGHT JOIN 'Suche' ON 'Stichwort'.'StichwortID' RIGHT JOIN 'Guide' ON 'Suche'.'GuideID';";
-            $sqlSearch = "SELECT Titel , Streetname , Bus_stop , Abstract , Google_Maps FROM Guide g INNER JOIN Suche su INNER JOIN Stichwort st ON g.GuideID = su.GuideID AND su.StichwortID = st.StichwortID WHERE st.Keywords = '$sucht';";
+            $sqlSearch = "SELECT Titel , Streetname , Bus_stop , Abstract , Google_Maps FROM Guide g INNER JOIN Suche su INNER JOIN Stichwort st ON g.GuideID = su.GuideID AND su.StichwortID = st.StichwortID WHERE st.Keywords = '$sucht';"; // SQl Search Query
             $tabelle = mysqli_query($db, $sqlSearch);
             while ($fetch = mysqli_fetch_array($tabelle)) {
                 print "<div class=\"attraktion\">
@@ -64,7 +64,7 @@ if (isset($_POST["Suche"])) $gesucht = true;
                         <p><b>Nächste Bushaltestelle:</b><br> ".$fetch['Bus_stop']."</p><br>
                         <p><b>Kurzbeschreibung:</b><br>".$fetch['Abstract']."</p><br>
                         <p><b>Auf der Karte:</b><br> ".$fetch['Google_Maps']."</p>
-                    </div>";
+                    </div>"; // Print all Search Results like below
             }
         }
     	?>
