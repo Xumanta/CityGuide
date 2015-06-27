e755dd8eb2575219f0fd5038a642843f19e04275
Xumanta <Xumanta@users.noreply.github.com>
2015-06-27 12:51:15 +0200

    Add Comments

--- adminen_stich.php
+++ adminen_stich.php
@@ -1,16 +1,15 @@
 <?php
 set_include_path('inc');
-$mysql_database = "City_GuideEn";
-include "db_connect.inc.php";
-include "function_save_sql.php";
+$mysql_database = "City_GuideEn";// Set used Database for Output
+include "db_connect.inc.php";	 // Universal connect
+include "function_save_sql.php"; // For secure Database Queries
 
-if(isset($_POST['keyword']))
-{
-	$stich = save_sql($_POST['keyword']);
+if(isset($_POST['keyword'])) {
+	$stich = save_sql($_POST['keyword']); // If a Keyword was added
 	$sqlEintrag = "INSERT INTO `Stichwort` (`StichwortID`, `Keywords`) VALUES ('NULL', '$stich')";
 	mysqli_query($db, $sqlEintrag);
 
-	header("Location: adminen_stich.php");
+	header("Location: adminen_stich.php");/ / Header on this File for cleaning the Post cache
 }
 ?>
 <!DOCTYPE html>
@@ -35,7 +34,7 @@ if(isset($_POST['keyword']))
 			while ($fetched = mysqli_fetch_array($tabelle))
 			{
 				print "$fetched['Keywords'] <br/>";
-			}
+			} // Prints all possible Keywords
 		?>
 	</body>
 </html>
